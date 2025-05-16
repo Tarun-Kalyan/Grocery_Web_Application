b3299e528c0dfa718329a9b8c194700796642a5e
Tarun kalyan <bodalatarun@gmail.com>
2025-05-16 13:34:34 +0530

    Delete grocery-app-frontend directory

--- grocery-app-frontend/src/app/models/app-user.model.ts
+++
@@ -1,6 +0,0 @@
-export interface AppUser {
-    id?: number;
-    username: string;
-    password?: string;
-  }
-  
--- grocery-app-frontend/src/app/models/grocery-item.model.ts
+++
@@ -1,5 +0,0 @@
-export interface GroceryItem {
-    id?: number;
-    name: string;
-    price: number;
-}
